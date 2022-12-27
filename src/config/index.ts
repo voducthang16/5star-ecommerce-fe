7783f0c53285c582f1ace49f6f2bfdbbacb36cce
Tran Phat <phat.trantan@h2-inc.com>
2022-12-27 08:44:59 +0700

    fix category

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const Config = {
-    apiUrl: 'http://5starfashion.xyz/api/',
-    // apiUrl: 'http://localhost:4000/',
+    // apiUrl: 'http://5starfashion.xyz/api/',
+    apiUrl: 'http://localhost:4000/',
 };
 export default Config;
